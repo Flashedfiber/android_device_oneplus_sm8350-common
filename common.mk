6d49678c797c47065845e8978ccbd3ea015e196d
TogoFire <togofire@mailfence.com>
2024-11-14 23:54:21 -0300

    sm8350-common: Drop wvmkiller
    F/DEBUG   ( 1459): Process uptime: 1s
    F/DEBUG   ( 1459): Cmdline: /vendor/bin/wvmkiller
    F/DEBUG   ( 1459): pid: 1406, tid: 1406, name: wvmkiller  >>> /vendor/bin/wvmkiller <<<
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: I054b382233b2e797c4f465b6a6f44f4f2ae3bf2a
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- common.mk
+++ common.mk
@@ -127,8 +127,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm-service.clearkey \
-    wvmkiller
+    android.hardware.drm-service.clearkey
 
 # Fastboot
 PRODUCT_PACKAGES += \

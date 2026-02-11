b1a4768bbff99cb178fb359a852fac6a73ecc558
Jammy555 <prathap.venkata@gmail.com>
2026-02-11 16:45:35 +0530

    Revert "sm8350-common: Introduce Dolby Atmos support"
    This reverts commit 837cd8b3845b5d2c90c134e8eea5fbc75facd281.
    
    Change-Id: I9949aee1804b1c7df53ef5a109a0d6cd2993d933

--- extras.mk
+++ extras.mk
@@ -6,3 +6,10 @@ TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
 # Dolby 
 $(call inherit-product, hardware/oplus/dolby/dolby.mk)
+
+# powerhal properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    pm.sleep_mode=1
+
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.post_boot.parsed=1

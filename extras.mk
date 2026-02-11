fadd5eb8a32e4a4fb84932711be31811de52461c
Jammy555 <prathap.venkata@gmail.com>
2026-02-11 17:17:54 +0530

    sm8350-common: Tune system properties for performance and rendering
    * Switch RenderEngine backend to SkiaGL Threaded to reduce jank
      and improve UI rendering performance.
    * Mention Enable GL backpressure.
    * Optimize EGL blobcache (multifile) to improve shader compilation
      and reduce stuttering.
    * Disable IORap as it is deprecated and introduces unnecessary overhead.
    * Enable system-side scroll optimizations for smoother scrolling
      in heavy applications.
    
    Change-Id: I9624b016a657a9f633afece24bc97204aeec2581
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>

--- extras.mk
+++ extras.mk
@@ -9,3 +9,20 @@ $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 # Dolby 
 $(call inherit-product, hardware/oplus/dolby/dolby.mk)
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.egl.blobcache.multifile=true \
+    ro.egl.blobcache.multifile_limit=33554432 \
+    debug.sf.disable_backpressure=0 \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.hwui.renderer=skiagl \
+    debug.renderengine.backend=skiaglthreaded 
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    renderthread.skia.reduceopstasksplitting=true
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.perf.scroll_opt=true \
+    persist.sys.perf.scroll_opt.heavy_app=1 \
+    ro.iorapd.enable=false \
+    persist.device_config.runtime_native_boot.iorap_readahead_enable=false
